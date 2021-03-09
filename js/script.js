fbf6a7c7e53268309612ca7d6cfe581c1c7b934c
prommpd830 <andimayapada04@gmail.com>
2021-03-09 18:32:54 +0700

    third commit

--- js/script.js
+++ js/script.js
@@ -21,39 +21,38 @@ M.ScrollSpy.init(elems, {
 });
 
 // Form Order
-$(document).on('click','.send_form', function(){
-var input_blanter = document.getElementById('icon_email');
+$(document).on('submit','.send_form', function(e){
+    e.preventDefault();
+    var input_blanter = document.getElementById('icon_email');
 
-/* Whatsapp Settings */
-var walink = 'https://web.whatsapp.com/send',
-    phone = '6285524569631',
-    walink2 = 'Hello There!';
+    /* Whatsapp Settings */
+    var walink = 'https://web.whatsapp.com/send',
+        phone = '6285524569631',
+        walink2 = 'Hello There!';
 
-/* Smartphone Support */
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-var walink = 'whatsapp://send';
-}
+    /* Smartphone Support */
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var walink = 'whatsapp://send';
+    }
 
-if("" != input_blanter.value){
+    if("" != input_blanter.value){
 
- /* Call Input Form */
+         /* Call Input Form */
 
-var input_name1 = $("#icon_prefix").val(),
-    input_email1 = $("#icon_email").val(),
-    input_number1 = $("#icon_telephone").val(),
-    input_textarea1 = $("#icon_message").val();
+        var input_name1 = $("#icon_prefix").val(),
+            input_email1 = $("#icon_email").val(),
+            input_number1 = $("#icon_telephone").val(),
+            input_textarea1 = $("#icon_message").val();
 
-/* Final Whatsapp URL */
-var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
-    '*Name* : ' + input_name1 + '%0A' +
-    '*Email Address* : ' + input_email1 + '%0A' +
-    '*Phone* : ' + input_number1 + '%0A' +
-    '*Message* : ' + input_textarea1 + '%0A';
+        /* Final Whatsapp URL */
+        var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
+            '*Name* : ' + input_name1 + '%0A' +
+            '*Email Address* : ' + input_email1 + '%0A' +
+            '*Phone* : ' + input_number1 + '%0A' +
+            '*Message* : ' + input_textarea1 + '%0A';
 
-/* Whatsapp Window Open */
-window.open(blanter_whatsapp,'_blank');
-M.toast({html: '<span class="red-text text-darken-1">The Form has been submitted succesfully!</span>'});
-} else {
-M.toast({html: '<span class="green-text text-accent-3">Fill in all Forms correctly!</span>'});
-} 
+        /* Whatsapp Window Open */
+        window.open(blanter_whatsapp,'_blank');
+            alert('The Form has been submitted succesfully!');
+    }
 });
